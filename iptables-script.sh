3a1a518a2dec018b2a2b85ae20c8a21018bed8a3
flpp-oliveira <felippeprofissionalsi@gmail.com>
2023-02-10 14:07:36 -0300

    Add script

---
+++ iptables-script.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# bloqueia conexões de entrada ssh 
+iptables -A INPUT -p tcp --dport 22 -j DROP
+
+# permite conexões de saida ssh
+iptables -A OUTPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+
+# bloqueia trafego não solicitado 
+iptables -A INPUT -m state --state INVALID -j DROP
+
+# bloqueia ataques de negação de serviço DoS e Ping Flood
+iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
+
+# Bloqueia ataques de spoofing
+iptables -A INPUT -s 127.0.0.0/8 -j DROP
+iptables -A INPUT -s 169.254.0.0/16 -j DROP
+iptables -A INPUT -s 172.16.0.0/12 -j DROP
+iptables -A INPUT -s 192.0.2.0/24 -j DROP
+iptables -A INPUT -s 224.0.0.0/4 -j DROP
+iptables -A INPUT -d 224.0.0.0/4 -j DROP
+iptables -A INPUT -s 240.0.0.0/5 -j DROP
+iptables -A INPUT -d 240.0.0.0/5 -j DROP
+iptables -A INPUT -s 0.0.0.0/8 -j DROP
+iptables -A INPUT -d 0.0.0.0/8 -j DROP
+iptables -A INPUT -d 239.255.255.0/24 -j DROP
+iptables -A INPUT -d 255.255.255.255 -j DROP
+
+# Bloqueia ataques de SYN Flood
+iptables -A INPUT -p tcp --syn -m limit --limit 1/s -j ACCEPT
+iptables -A INPUT -p tcp --syn -j DROP
+
+# salvar no iptables rules
+iptables-save > /etc/iptables.rules
